6a434c3505adb3097e65d0fe4b20513157f75de7
Max Janorschke <mm.madmax2506@gmail.com>
2020-08-30 20:50:01 +0200

    Bildanalyse

---
+++ image-metadata/analysis.py
@@ -0,0 +1,19 @@
+from PIL import Image, ExifTags
+from helper.files import get_path_to_file, get_filename
+
+def analysis(image_file):
+    output_path = get_path_to_file(image_file) + "output-from-" + get_filename(image_file) + ".txt"
+
+    img = Image.open(image_file)
+    file = open(output_path, "w")
+    for i, j in img._getexif().items():
+        if i in ExifTags.TAGS:
+            try:
+                data = ExifTags.TAGS[i] + " - " + j.decode("utf-8")
+            except:
+                data = ExifTags.TAGS[i] + " - " + str(j)
+            file.write(data + "\n")
+            print(data)
+
+
+analysis("files/picture.jpg")
